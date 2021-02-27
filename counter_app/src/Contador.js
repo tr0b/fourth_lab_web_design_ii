1d450d9376f74ceda2ad3948ee9c498fc922ac2b
tr0b <trobinson3000600@gmail.com>
2021-02-27 13:31:04 -0600

    Fourth Lab Web Design II - Will Add substract button Changes: - Added substract (-) button to Contador component - Added on substract logic to Contador component (decrementarContador)

--- counter_app/src/Contador.js
+++ counter_app/src/Contador.js
@@ -6,18 +6,28 @@ export default class Contador extends Component {
     this.state = { cuenta: 0 };
     //this.incremetarContador = this.incremetarContador.bind(this);
   }
+  /** @description Increments counter by one unit. (Incrementa el contador por una unidad)  */
   incremetarContador() {
     this.setState({
       cuenta: this.state.cuenta + 1
+    });
+  }
+
+/** @description Decrements counter by one unit. (Decrementa el contador por una unidad) */
+decrementarContador() {
+    this.setState({
+      cuenta: this.state.cuenta - 1
     });
   }
   render() {
     return (
       <div>
         <h1>Contador</h1>
-        <p>Este componente va incrementar el valor en 1</p>
+        <p>This component can add or substract one unit to the counter</p>
+        <p>Este componente puede sumar o restar una unidad al contador</p>
         <p>Cuenta:{this.state.cuenta}</p>
         <button onClick={this.incremetarContador.bind(this)}>+</button>
+        <button onClick={this.decrementarContador.bind(this)}>-</button>
       </div>
     );
   }
